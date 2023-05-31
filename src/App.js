3035dd27fe6deaea7815869911bc173929f7d7d8
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-05-31 10:58:14 +0530

    Data fetching and displaying using fetch api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((json) => setUsers(json))
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {users.map((user) => (
+      <div className="card">
+        <p>{user.name}</p>
+        <p>{user.userName}</p>
+      </div>
+    ))}
+    </>
   );
 }
 
